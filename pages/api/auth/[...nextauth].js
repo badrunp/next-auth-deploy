6ea66eaa112be64e564c10e659c040684d7f64ec
= <bbadrunn@gmail.com>
2022-07-07 08:29:42 +0700

    initial commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Username",
+        },
+        password: {
+          label: "Password",
+          type: "text",
+          placeholder: "Password",
+        },
+      },
+      async authorize() {
+        const user = { id: 1, name: "badrun", email: "bbadrunn@gmail.com" };
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+});
